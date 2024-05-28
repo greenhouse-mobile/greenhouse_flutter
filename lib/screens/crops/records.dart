7b9a4158cb80f77fbbfaa71db9504773e965e249
trabajosUPC-AlanGalavis <u202110223@upc.edu.pe>
2024-05-27 21:19:44 -0500

    feat(records): Added create record button

--- lib/screens/crops/records.dart
+++ lib/screens/crops/records.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:greenhouse/models/crop_phase.dart';
 import 'package:greenhouse/widgets/bottom_navigation_bar.dart';
+import 'package:greenhouse/widgets/message_response.dart';
 import 'package:greenhouse/widgets/record_card.dart';
 
-class RecordsScreen extends StatelessWidget {
+class RecordsScreen extends StatefulWidget {
   RecordsScreen({super.key});
 
-  final List<RecordCard> recordCards = [
+  @override
+  State<RecordsScreen> createState() => _RecordsScreenState();
+}
+
+class _RecordsScreenState extends State<RecordsScreen> {
+  var cropId = '1';
+  var cropPhase = 'Preparation Area';
+  List<RecordCard> recordCards = [
     RecordCard(
       entryDate: '2021-08-01',
       recordId: '124234',
@@ -26,23 +35,58 @@ class RecordsScreen extends StatelessWidget {
     ),
   ];
 
+  void addNewRecord() {
+    setState(() {
+      recordCards.add(RecordCard(
+        entryDate: '2021-08-01',
+        recordId: '${recordCards.length + 1}',
+        author: 'Winston Smith',
+        currentPhase: 'Tunnel',
+      ));
+      recordCards = recordCards;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Records'),
+          title: Text('Go Back', style: TextStyle(fontSize: 16)),
         ),
-        body: ListView.builder(
-          itemCount: recordCards.length,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.only(bottom: 20.0),
-              child: GestureDetector(
-                onTap: () {},
-                child: recordCards[index],
-              ),
-            );
+        body: ListView(
+          children: [
+            Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'Crop ID: $cropId',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF4C6444)),
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      cropPhase,
+                      style: TextStyle(fontSize: 16),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                )),
+            ...recordCards,
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            messageResponse(
+                context,
+                "Are you sure you want \nto create a record?",
+                "Confirm",
+                addNewRecord);
           },
+          backgroundColor: Color(0xFFB07D50),
+          child: Icon(Icons.add, color: Colors.white),
         ),
         bottomNavigationBar: GreenhouseBottomNavigationBar());
   }
