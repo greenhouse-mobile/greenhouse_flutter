ef50623937506b61d8eff9ba32eb8b6714b886d9
Lucero Obispo Rios <u202111465@upc.edu.pe>
2024-06-26 12:36:29 -0500

    feat: Add Temperature control in pausterization tunnel graphic

--- lib/screens/crops/statistical_reports.dart
+++ lib/screens/crops/statistical_reports.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:greenhouse/models/crop.dart';
 import 'package:greenhouse/widgets/bottom_navigation_bar.dart';
 import 'package:greenhouse/widgets/line_chart.dart';
+import 'package:greenhouse/widgets/multiline_chart.dart';
 
 class StatisticalReports extends StatefulWidget {
   final Crop crop;
@@ -19,22 +20,147 @@ class _StatisticalReportsState extends State<StatisticalReports> {
       appBar: AppBar(
         title: Text('Go Back', style: TextStyle(fontSize: 16)),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                'Statistical Reports',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  'Statistical Reports',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
               ),
-            ),
-            LineChartSample4(),
-          ],
+              Padding(
+                padding: const EdgeInsets.only(left: 16.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Phase 1: Bunker temperature control',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ),
+              LineChartSample4(),
+              SizedBox(height: 50),
+              Padding(
+                padding: const EdgeInsets.only(left: 16.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Phase 2: Temperature control in pausterization tunnel',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ),
+              legendItem(),
+              LineChartSample1(),
+              SizedBox(height: 50),
+              Padding(
+                padding: const EdgeInsets.only(left: 16.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Phase 3: ',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: GreenhouseBottomNavigationBar(),
     );
   }
+}
+
+Widget legendItem() {
+  return Padding(
+    padding: const EdgeInsets.all(15.0),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  width: 20,
+                  height: 20,
+                  color: Colors.blue,
+                ),
+                const SizedBox(width: 5),
+                const Text('Ct1'),
+              ],
+            ),
+            const SizedBox(height: 5),
+            Row(
+              children: [
+                Container(
+                  width: 20,
+                  height: 20,
+                  color: Colors.red,
+                ),
+                const SizedBox(width: 5),
+                const Text('Ct2'),
+              ],
+            ),
+            const SizedBox(height: 5),
+            Row(
+              children: [
+                Container(
+                  width: 20,
+                  height: 20,
+                  color: Color.fromARGB(255, 168, 243, 170),
+                ),
+                const SizedBox(width: 5),
+                const Text('Ct3'),
+              ],
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  width: 20,
+                  height: 20,
+                  color: const Color.fromRGBO(255, 165, 0, 1),
+                ),
+                const SizedBox(width: 5),
+                const Text('Ct4'),
+              ],
+            ),
+            const SizedBox(height: 5),
+            Row(
+              children: [
+                Container(
+                  width: 20,
+                  height: 20,
+                  color: Color.fromRGBO(84, 235, 255, 1),
+                ),
+                const SizedBox(width: 5),
+                const Text('At'),
+              ],
+            ),
+            const SizedBox(height: 5),
+            Row(
+              children: [
+                Container(
+                  width: 20,
+                  height: 20,
+                  color: Color.fromARGB(255, 227, 255, 18),
+                ),
+                const SizedBox(width: 5),
+                const Text('At'),
+              ],
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
 }
